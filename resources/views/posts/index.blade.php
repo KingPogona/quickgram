9883639b4cc7519ed717609a1262f2173e957885
KingPogona <31211822+KingPogona@users.noreply.github.com>
2022-03-15 19:12:34 -0600

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,34 +3,42 @@
 @section('content')
     <div class="container">
 
-        @foreach ($posts as $post)
+        @if (count($posts) < 1)
+            <div class="d-flex justify-content-center">
+                <div>
+                    <p>Your feed is empty. Try searching for some creators!</p>
+                </div>
+            </div>
+        @else
+            @foreach ($posts as $post)
+                <div class="row">
+                    <div class="col-6 offset-3">
+                        <a href="/profile/{{ $post->user->id }}">
+                            <img src="/storage/{{ $post->image }}" alt="" class="w-100">
+                        </a>
+                    </div>
+                </div>
+                <div class="row pt-2 pb-4">
+                    <div class="col-6 offset-3">
+
+                        <p>
+                            <span class="fw-bold">
+                                <a href="/profile/{{ $post->user->id }}"
+                                    class="text-decoration-none text-dark">{{ $post->user->username }}
+                                </a>
+                            </span>
+                            {{ $post->caption }}
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+
             <div class="row">
-                <div class="col-6 offset-3">
-                    <a href="/profile/{{ $post->user->id }}">
-                        <img src="/storage/{{ $post->image }}" alt="" class="w-100">
-                    </a>
+                <div class="col-12 d-flex justify-content-center">
+                    {{ $posts->links() }}
                 </div>
             </div>
-            <div class="row pt-2 pb-4">
-                <div class="col-6 offset-3">
-
-                    <p>
-                        <span class="fw-bold">
-                            <a href="/profile/{{ $post->user->id }}"
-                                class="text-decoration-none text-dark">{{ $post->user->username }}
-                            </a>
-                        </span>
-                        {{ $post->caption }}
-                    </p>
-                </div>
-            </div>
-        @endforeach
-
-        <div class="row">
-            <div class="col-12 d-flex justify-content-center">
-                {{ $posts->links() }}
-            </div>
-        </div>
+        @endif
 
     </div>
 @endsection
